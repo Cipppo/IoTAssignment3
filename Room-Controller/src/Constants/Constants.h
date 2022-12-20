40e51bda7945e69118b41dc041e98c1c1a0c99d3
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-12-20 18:16:08 +0100

    Implemented Constants file in order to keep track the constants

---
+++ Room-Controller/src/Constants/Constants.h
@@ -0,0 +1,18 @@
+#ifndef __CONSTANTS__
+#define __CONSTANTS__
+
+
+
+//Servo Motor
+#define ZERO_SERVO 550
+#define FLAT_ANGLE_SERVO 2300
+#define FLAT_ANGLE 180
+
+//Photores
+#define FREQUENCY 5
+#define PERIOD 1000/FREQUENCY
+
+
+
+
+#endif
