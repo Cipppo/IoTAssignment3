d8ceda17a4031bb58f30754f4fdcbe2a54610f99
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-12-21 17:38:23 +0100

    Implemented api/led and api/slider routes

--- Room-Service/server.py
+++ Room-Service/server.py
@@ -1,16 +1,28 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 
 app = Flask(__name__)
 
+
+visited = 0
+
 @app.route("/")
 def home():
-    return "Hello, World!"
+    global visited
+    visited = visited + 1
+    return "Hello, World!" + str(visited)
+ 
 
+@app.route("/api/led")
+def led():
+    status = request.args.get("LED")
+    return status
 
+@app.route("/api/slider", methods=['POST'])
+def slider():
+    content = request.json
+    print(content["value"])
+    return content["value"]
 
-
-
-
-app.run()
+app.run(port = 5555)
