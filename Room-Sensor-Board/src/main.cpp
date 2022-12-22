15c070a334a4259e1134c997b05fee9189ef6034
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-12-22 17:45:18 +0100

    testing mqtt, need to test this

--- Room-Sensor-Board/src/main.cpp
+++ Room-Sensor-Board/src/main.cpp
@@ -2,12 +2,24 @@
 
 
 
+#include "WiFiNode/WiFiNode.h"
+#include "Constants/Constants.h"
+#include "MQTTClient/MQTTClient.h"
+
+WiFiNode wifi = WiFiNode("FBIVan", "12345678");
+MQTTClient mqttclient = MQTTClient("broker.mqtt-dashboard.com", "light/mqtt", 1883);
 
 void setup() {
-  Serial.begin(115200);
+    Serial.begin(9600);
+    wifi.connect();
+    mqttclient.setup();
 }
 
 void loop() {
-  Serial.println("dioporco");
-  delay(1000);
+    if(!mqttclient.isClientConnected()){
+        mqttclient.connect();
+    }else{
+        mqttclient.sendMessage("dp");
+    }
+    delay(1000);
 }
