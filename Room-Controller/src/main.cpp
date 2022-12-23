facb8e2605c9f6023b867a4972abc9ddc53c35bc
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-12-23 16:43:57 +0100

    Trying

--- Room-Controller/src/main.cpp
+++ Room-Controller/src/main.cpp
@@ -1,7 +1,10 @@
 #include <Arduino.h>
 
+#include "Constants/Constants.h"
+#include "Hardware/Led/Led.h"
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
 }
 
 void loop() {
