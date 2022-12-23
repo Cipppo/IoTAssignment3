47c0bfe2d3721a44b4480e35acbbec1d2548d77e
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-12-23 13:36:07 +0100

    Add constants

--- Room-Sensor-Board/src/Constants/Constants.h
+++ Room-Sensor-Board/src/Constants/Constants.h
@@ -2,8 +2,13 @@
 #define __CONSTANTS__
 
 //---WIFI---
-
+#define SSID "FBIVan"
+#define PASSWORD "12345678"
 
 //--MQTT--
 #define RECONNECTION_DELAY 3000
+#define MQTT_SERVER "broker.mqtt-dashboard.com"
+#define TOPIC "light/mqtt"
+#define PORT 1883
+
 #endif
