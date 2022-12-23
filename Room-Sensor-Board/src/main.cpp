76a6a48569c2346dffb0c5b5e692d99d22de3a2d
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-12-23 16:43:28 +0100

    trying

--- Room-Sensor-Board/src/main.cpp
+++ Room-Sensor-Board/src/main.cpp
@@ -5,24 +5,36 @@
 #include "model/wifiNode/WiFiNode.h"
 #include "constants/Constants.h"
 #include "model/mqttClient/MQTTClient.h"
+#include "model/hardware/pir/Pir.h"
 
+Pir* pir;
 
-
-WiFiNode* wifi;
-MQTTClient* mqttClient;
 
 
 
 
 void setup() {
-  Serial.begin(9600);
-  wifi = new WiFiNode();
-  mqttClient = new MQTTClient();
+    Serial.begin(9600);
+    pinMode(PIR_PIN,INPUT);
+
+    
+    //give the sensor some time to calibrate
+    Serial.print("Calibrating sensor... ");
+    for(int i = 0; i < CALIBRATION_TIME; i++){
+        Serial.print(".");
+        delay(1000);
+    }
+    Serial.println(" done");
+    Serial.println("PIR SENSOR READY.");
+    delay(50);
+
 }
 
 void loop() {
-
-    mqttClient->sendMessage("messaggio");
-    delay(1000);
+    
+  int detected = digitalRead(PIR_PIN);
+  if (detected == HIGH){
+    Serial.println("detected!");  
+  }
 
 }
