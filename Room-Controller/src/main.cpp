24182bc19ec66655d6c32fb0c1a28b2bcc49ec70
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-12-26 18:57:27 +0100

    Trying the communication between Arduino and Python

--- Room-Controller/src/main.cpp
+++ Room-Controller/src/main.cpp
@@ -1,12 +1,31 @@
 #include <Arduino.h>
+#include <ArduinoJson.h>
 
 #include "Constants/Constants.h"
 #include "Hardware/Led/Led.h"
+#include "DataSerialization/DataDeSerializer.h"
+
+Led* led;
+DataDeSerializer deSerializer;
 
 void setup() {
   Serial.begin(9600);
+  led = new Led(LED_PIN);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  String line = Serial.readStringUntil("\n");
+  if(line != ""){
+    deSerializer.deSerialize(line);
+
+    int valore = deSerializer.get("value");
+
+    if(valore == 0 && led->readValue() == HIGH){
+      digitalWrite(led->getPin(), LOW);
+    }else if (valore == 1 && led->readValue() == LOW) {
+      digitalWrite(led->getPin(), HIGH);
+    }
+  }
+
+
 }
