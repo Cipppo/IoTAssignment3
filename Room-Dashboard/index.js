beda432770446ec2598383a14123c954b8983520
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-12-27 14:56:38 +0100

    Change post request method

--- Room-Dashboard/index.js
+++ Room-Dashboard/index.js
@@ -18,7 +18,7 @@ let data = document.getElementById('Data')
 
 spegni.addEventListener("click", function(e){
     console.log("a");
-    spegni.className = "btn btn-success";
+    spegni.className = "btn btn-danger";
     accendi.className = "btn btn-dark"; 
     axios.get('http://127.0.0.1:5555/api/led?LED=0');
 
@@ -32,12 +32,18 @@ accendi.addEventListener("click", function(e){
 })
 
 invia.addEventListener("click", function(e){
-    val = slider.value;
+    let val = slider.value
     axios.post('http://127.0.0.1:5555/api/slider', {
-        "value": "10"
-    });
+        method: 'POST', 
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            value: val
+        })
+    })
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
 })
-
-
 
 setInterval(updateClock, 1000);
