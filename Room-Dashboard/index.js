109bf1854b5ef35317b349bfc5e5e41b64915873
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-12-28 01:23:26 +0100

    Add Frontside Backup methods

--- Room-Dashboard/index.js
+++ Room-Dashboard/index.js
@@ -12,9 +12,18 @@ function updateClock() {
 
 let accendi = document.getElementById('Accendi');
 let spegni = document.getElementById('Spegni');
+
 let invia = document.getElementById('Invia');
 let slider = document.getElementById('Slider');
-let data = document.getElementById('Data')
+
+let data = document.getElementById('Data');
+
+let apriTutto = document.getElementById('ApriTutto');
+let chiudiTutto = document.getElementById('ChiudiTutto');
+
+let timeSet0 = document.getElementById('timeSet8');
+let timeSet19 = document.getElementById('timeSet19');
+
 
 spegni.addEventListener("click", function(e){
     console.log("a");
@@ -44,6 +53,22 @@ invia.addEventListener("click", function(e){
     })
     .then(data => console.log(data))
     .catch(error => console.error(error));
+})
+
+apriTutto.addEventListener('click', function(e){
+    axios.get('http://127.0.0.1:5555/api/servoDebug?ALPHA=0');
+})
+
+chiudiTutto.addEventListener('click', function(e){
+    axios.get('http://127.0.0.1:5555/api/servoDebug?ALPHA=0');
+})
+
+timeSet0.addEventListener('click', function(e){
+    axios.get('http://127.0.0.1:5555/api/timeDebug?TIME=8');
+})
+
+timeSet19.addEventListener('click', function(e){
+    axios.get('http://127.0.0.1:5555/api/timeDebug?TIME=19');
 })
 
 setInterval(updateClock, 1000);
