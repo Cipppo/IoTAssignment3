b7777d57df2f874beb0fc4b2cd0de1210835a591
Luca Rapolla <luca.rapolla@studio.unibo.it>
2023-01-09 14:45:34 +0100

    Example method for send and receive JSON Through the serial line

--- Room-Service/trySerial.py
+++ Room-Service/trySerial.py
@@ -6,10 +6,10 @@ import threading
 ser = serial.Serial(port="/dev/ttyACM0", baudrate=9600)
 
 
-def create_JSON_data(component, value):
+def create_JSON_data(lightVal, servoVal):
     data = {
-        "component": component, 
-        "value": value,
+        "Light": lightVal, 
+        "Servo": servoVal,
     }
     json_str = json.dumps(data)
     #print(json_str + '\n')
@@ -21,21 +21,28 @@ def create_JSON_data(component, value):
 def handleReceive():
     while True:
 
-        line = ser.readline()
-        line_s = line.decode()
-        print("Ho tirato su questo dato: "+line_s)
-        time.sleep(0.2)
+        if(ser.inWaiting() > 0):
+            try:
+                msg = ser.readline().decode()
+                json_parsed = json.loads(msg)
+                value = json_parsed['value']
+                print(value)
+            except Exception as e:
+                print("Rotto")
+        time.sleep(0.5)
 
 def handleSend():
-    while True:
-        data = create_JSON_data("lights", 1)
+        data = create_JSON_data(50, 80)
+        print("Sending: " + data)
         ser.write(data.encode())
         time.sleep(0.2)
 
 
 def run():
     #threading.Thread(target=handleReceive).start()
-    threading.Thread(target=handleSend).start()
+    #threading.Thread(target=handleSend).start()
+    handleSend()
 
 
 run()
+
