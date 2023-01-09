68d72dff111f8c87ed456131e3309411aaf347d6
Luca Rapolla <luca.rapolla@studio.unibo.it>
2023-01-09 14:47:49 +0100

    Deleted some method bodies, maybe gonna delete asap

--- Room-Controller/src/SerialCommunication/SerialComsHandler.cpp
+++ Room-Controller/src/SerialCommunication/SerialComsHandler.cpp
@@ -4,17 +4,7 @@
 
 
 bool SerialComsHandler::read(){
-    String line = Serial.readStringUntil('\n');
-    if(line != ""){
-        this->deSerializer.deSerialize(line);
-        this->part = this->deSerializer.get("component");
-        this->value = this->deSerializer.get("value");
-        Serial.println(value);
-        return true;
-    }else{
-        //Serial.println("no");
-        return false;
-    }
+
 }
 
 String SerialComsHandler::getPart(){
@@ -26,8 +16,7 @@ int SerialComsHandler::getValue(){
 }
 
 void SerialComsHandler::write(String key, int value){
-    String line = this->serializer.serialize(key, value);
-    Serial.println(line);
+    
 }
 
 void SerialComsHandler::writeStr(String string){
