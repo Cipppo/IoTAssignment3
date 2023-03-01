dc600c3b6224619ba0bb49e1ebbfb1c9cc9a8061
Luca Rapolla <luca.rapolla@studio.unibo.it>
2023-03-01 16:01:43 +0100

    Resetting

--- Room-Controller/src/main.cpp
+++ Room-Controller/src/main.cpp
@@ -5,30 +5,24 @@
 #include "Hardware/Led/Led.h"
 #include "DataSerialization/DataDeSerializer.h"
 #include "Hardware/Photores/Photores.h"
-#include "SerialCommunication/SerialComsHandler.h"
+#include "SerialCommunication/SerialMsgHandler.h"
 #include "DataSerialization/ReceiveDatagram.h"
 #include "Controller/Controller.h"
 
 #define PIN 2
 
 
-
+SerialMsgHandler* msgHandler;
 Controller* controller;
 
 
-void changeTheState(){
-  controller->getTimer()->changeState();
-}
+
 
 void setup(){
-  Serial.begin(9600);
-  controller = new Controller();
-  controller->init();
-  controller->getTimer()->getTimerOne()->attachInterrupt(changeTheState);
+  msgHandler->init();
   //Serial.println("Tra 6 secondi il messaggio verra' letto");
 }
 
 void loop(){
-  controller->getTimer()->waitForTheNextTick();
-  controller->tick();
+  
 };
